0de9e977b4d13f29a773ed43e31fe4befa136a76
hsloot <Henrik.Sloot@gmail.com>
2020-10-17 21:38:54 +0000

    Deploying to gh-pages from  @ 56e6a58223f0a6fa4ddec799dc021647f6541780 🚀

--- test/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/cpp-ho-library-template/cpp-ho-library-template/test
-# Build directory: /home/runner/work/cpp-ho-library-template/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_all "/home/runner/work/cpp-ho-library-template/build/test/testlib")
-set_tests_properties(test_all PROPERTIES  _BACKTRACE_TRIPLES "/home/runner/work/cpp-ho-library-template/cpp-ho-library-template/test/CMakeLists.txt;17;add_test;/home/runner/work/cpp-ho-library-template/cpp-ho-library-template/test/CMakeLists.txt;0;")
-subdirs("../_deps/catch-build")
